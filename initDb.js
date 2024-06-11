1d7a03290ce52e4d1193994cab24e004d73f980c
Aakash Murthy <20072190+aakashmurthy@users.noreply.github.com>
2024-06-11 15:08:13 -0700

    Add initalize DB script

--- initDb.js
+++ initDb.js
@@ -1,2 +1,21 @@
 require('dotenv').config()
+const sequelize = require('./lib/sequelize')
 
+const { Assignment, AssignmentSchema } = require('./models/assignment')
+const { Course, CourseSchema } = require('./models/course')
+const { Submission, SubmissionSchema } = require('./models/submission')
+const { User, UserSchema } = require('./models/user')
+
+const assignmentData = require('./data/assignments.json')
+const courseData = require('./data/courses.json')
+const submissionData = require('./data/submissions.json')
+const userData = require('./data/users.json')
+
+
+sequelize.sync().then(async function () {
+    await User.bulkCreate(userData, { fields: UserSchema.keys })
+    await Course.bulkCreate(courseData, { fields: CourseSchema.keys })
+    await Assignment.bulkCreate(assignmentData, { fields: AssignmentSchema.keys })
+    await Submission.bulkCreate(submissionData, { fields: SubmissionSchema.keys })
+    console.log('Database populated')
+})

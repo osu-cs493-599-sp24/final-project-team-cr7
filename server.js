0ade98591a9e2cfef1d6ddf425f489388511f966
John Tran <johnyuritran@gmail.com>
2024-06-11 22:31:27 -0700

    Cleaning up server.js

--- server.js
+++ server.js
@@ -95,31 +95,3 @@ sequelize.sync().then(async function () {
         console.log("== Server is running on port", port)
     })
 })
-
-// const express = require('express');
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// // Use json middleware
-// app.use(express.json());
-
-// // Error handling
-// app.use((err, req, res, next) => {
-//     if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-//         console.error(err); // Log the error for debugging
-//         return res.status(500).json({
-//             error: 'Internal Server Error',
-//             message: 'An unexpected error occurred on the server',
-//         });
-//     } else {
-//         next();
-//     }
-// });
-
-// // Routes
-// app.use('/', require('./api'));
-
-// // Start server
-// app.listen(port, () => {
-//     console.log(`Server is running on port ${port}`);
-// });

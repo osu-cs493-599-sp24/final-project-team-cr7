717c67f0c95f90389c2cba9f619239b86071bd34
Aakash Murthy <20072190+aakashmurthy@users.noreply.github.com>
2024-06-12 14:18:09 -0700

    Reworked initDB to only add if table(s) are empty

--- initDb.js
+++ initDb.js
@@ -13,15 +13,25 @@ const userData = require('./data/users.json');
 const courseStudentData = require('./data/coursestudents.json');
 
 sequelize.sync().then(async function () {
-    await User.bulkCreate(userData, { fields: UserSchema.keys });
-    await Course.bulkCreate(courseData, { fields: CourseSchema.keys });
-    await Assignment.bulkCreate(assignmentData, {
-        fields: AssignmentSchema.keys,
-    });
-    await Submission.bulkCreate(submissionData, {
-        fields: SubmissionSchema.keys,
-    });
-    await CourseStudents.bulkCreate(courseStudentData);
-    console.log('Database populated');
+    if (!await User.count()) {
+        await User.bulkCreate(userData, { fields: UserSchema.keys });
+    }
+
+    if (!await Course.count()) {
+        await Course.bulkCreate(courseData, { fields: CourseSchema.keys });
+    }
+
+    if (!await Assignment.count()) {
+        await Assignment.bulkCreate(assignmentData, { fields: AssignmentSchema.keys });
+    }
+
+    if (!await Submission.count()) {
+        await Submission.bulkCreate(submissionData, { fields: SubmissionSchema.keys });
+    }
+
+    if (!await CourseStudents.count()) {
+        await CourseStudents.bulkCreate(courseStudentData);
+    }
+    console.log('=== Database populated');
 });
 
